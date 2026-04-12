25a4a3111fd0e2f32371415a6b94a598885e9ba5
Bruno Racobaldo Santos <60931988+racoba@users.noreply.github.com>
2026-04-12 11:01:47 -0300

    Enhance layout component with suppressHydrationWarning attribute
    - Added `suppressHydrationWarning` attribute to the `<html>` and `<body>` elements in `layout.tsx` to prevent hydration errors in Next.js applications.

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,8 +31,9 @@ export default function RootLayout({
     <html
       lang="pt-BR"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
         <MuiAppProvider>{children}</MuiAppProvider>
       </body>
     </html>
